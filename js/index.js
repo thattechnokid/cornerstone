583261923d34918410d76b3fe76327682b411a50
Cristian Mendoza <thattechnokid@gmail.com>
2018-03-03 02:33:40 -0800

    Making progress

--- js/index.js
+++ js/index.js
@@ -5,11 +5,31 @@ function shrinkNavOnScroll(){
 
   if (yDistance > shrinkBreakpoint) {
     navElement.classList.add('shrinked-nav');
-    document.querySelector('.js-nav').style.height = '60px'
+    document.querySelector('.js-nav').style.height = '60px';
+
+    document.querySelector('.brand').setAttribute('src','http://via.placeholder.com/150x60');
+
   }else {
     navElement.classList.remove("shrinked-nav");
-    document.querySelector('.js-nav').style.height = '100px'
+    document.querySelector('.js-nav').style.height = '100px';
+
+    document.querySelector('.brand').setAttribute('src','http://via.placeholder.com/150x100');
   }
 }
+
+var callBtn = document.querySelector('.call-btn'),
+callText = document.querySelector('.call-txt');
+
+callText.addEventListener('mouseenter',function(){
+  callBtn.classList.add('call-hover');
+  setTimeout(function(){
+    callText.style.display = 'none';
+  },250);
+});
+
+// callText.addEventListener('mouseout',function(){
+//   callBtn.classList.remove('call-hover');
+//
+// });
 
 window.addEventListener('scroll', shrinkNavOnScroll);
